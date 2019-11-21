4b388ac24d50109941cc6a58acd37f7995fe3902
Nat Weerawan <nat@cmmc.io>
2019-11-21 14:40:52 +0700

    update code

--- Yolo-digit-detector/opjdt5.py
+++ Yolo-digit-detector/opjdt5.py
@@ -32,3 +32,44 @@ yolo = create_yolo(config['model']['architecture'],
                    config['model']['input_size'],
                    config['model']['anchors'])
 yolo.load_weights('model.h5')
+
+class opimg(threading.Thread):
+
+    # Thread class with a _stop() method.
+    # The thread itself has to check
+    # regularly for the stopped() condition.
+
+    def __init__(self, *args, **kwargs):
+        super(CaptureImage, self).__init__(*args, **kwargs)
+        self._stop = threading.Event()
+        self._camera = CSICamera(width=224, height=224)
+        self._camera.running = True
+        print("camera was init :)")
+
+        # function using _stop function
+
+    def stop(self):
+        self._camera.running = False
+        self._stop.set()
+
+    def stopped(self):
+        return self._stop.isSet()
+
+    def run(self):
+        while True:
+            if self.stopped():
+                return
+            file = "dataset/{0}-{1}.jpg".format("A", time.time())
+            print(file)
+            cv2.imwrite(file, self._camera.value)
+            time.sleep(.2)
+
+    # def capture(self, cls, len):
+    #
+    #     # cv2.imwrite("dataset/{0}-{1}.jpg".format("A", time.time()), camera.value)
+    #     # cap = cv2.VideoCapture(0)
+    #     # print("myfunc started")
+    #     # for i in range(0, len):
+    #     #     ret, frame = cap.read()
+    #     #     cv2.imwrite("dataset/{0}/{1}.jpg".format(cls, time.time()), frame)
+    #     #     print("saved")
